6c9046445e6355ee08ed286e1c6eeaca0836bd14
marcos-jesus <marcosjesus.dev@gmail.com>
2023-01-15 14:21:03 -0300

    build Structure Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { styled } from '@/styles'
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
 import { Roboto } from '@next/font/google'
 
 const roboto = Roboto({
@@ -7,30 +8,33 @@ const roboto = Roboto({
   subsets:['latin']
 })
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  width: 80,
-  padding: ' 4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  },
-})
+import camiseta1 from '../assets/Camisetas/camiseta1.png'
+import camiseta2 from '../assets/Camisetas/camiseta2.png'
+import camiseta3 from '../assets/Camisetas/camiseta3.png'
 
 
 export default function Home() {
   return (
 
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer className={roboto.className}>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta Maratona Explorer Red</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta Maratona Explorer Blue</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
 
   )
 }
