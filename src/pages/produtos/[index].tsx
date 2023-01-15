ce476ab2aa83f9b315375fd452ce61da309e5127
marcos-jesus <marcosjesus.dev@gmail.com>
2023-01-14 22:58:26 -0300

    routes navigation next13

---
+++ src/pages/produtos/[index].tsx
@@ -0,0 +1,5 @@
+function ProdutoId() {
+  return <h1>Produto especifico</h1>
+}
+
+export default ProdutoId
---
+++ src/pages/produtos/index.tsx
@@ -0,0 +1,5 @@
+function Produtos() {
+  return <h1>Produtos de Marca</h1>
+}
+
+export default Produtos
