7f788a42730216510ed0e69cf8a89ef2d68c4597
marcos-jesus <marcosjesus.dev@gmail.com>
2023-01-14 22:28:18 -0300

    Next 13

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal']
+})
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ptBR">
+      <Head className={roboto.className} />
       <body>
         <Main />
         <NextScript />
