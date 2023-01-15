474a637cbd685388fd78b3536ee865da03a4da61
marcos-jesus <marcosjesus.dev@gmail.com>
2023-01-14 22:48:55 -0300

    Add Fonts next13

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Roboto } from '@next/font/google'
-
-const roboto = Roboto({
-  weight: ['400', '700'],
-  style: ['normal']
-})
 
 export default function Document() {
   return (
     <Html lang="ptBR">
-      <Head className={roboto.className} />
+      <Head />
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets:['latin']
+})
+
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return <h1 className={roboto.className}>Hello World</h1>
 }
