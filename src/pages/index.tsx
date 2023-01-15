8453aa4e252477d5cba846d0aa68f81287797c5d
marcos-jesus <marcosjesus.dev@gmail.com>
2023-01-15 15:12:52 -0300

    build Carousel products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 import { HomeContainer, Product } from '../styles/pages/home'
 import { Roboto } from '@next/font/google'
 
+import { useKeenSlider} from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
 const roboto = Roboto({
   weight: ['400', '700'],
   style: ['normal'],
@@ -14,26 +17,51 @@ import camiseta3 from '../assets/Camisetas/camiseta3.png'
 
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 38
+    }
+  })
   return (
 
-    <HomeContainer className={roboto.className}>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta1} width={520} height={480} alt='' />
 
         <footer>
-          <strong>Camiseta Maratona Explorer Red</strong>
+          <strong>Camiseta Explorer Blue</strong>
           <span> R$ 79,90</span>
         </footer>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta2} width={520} height={480} alt='' />
 
         <footer>
-          <strong>Camiseta Maratona Explorer Blue</strong>
+          <strong>Camiseta Explorer Red</strong>
           <span> R$ 79,90</span>
         </footer>
       </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={401} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta Ignite lab</strong>
+          <span> R$ 99,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta2} width={401} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta Explorer Red Premium</strong>
+          <span> R$ 99,90</span>
+        </footer>
+      </Product>
+      
     </HomeContainer>
 
   )
